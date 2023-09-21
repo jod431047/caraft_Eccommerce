331d4d331ee0de496db46368128eee031788227b
jod431047 <jod431047@gmail.com>
2023-09-22 00:18:18 +0200

    add templates in my project

--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path 
 from django.conf import settings
 from django.conf.urls.static import static
+from caraft.views import CaraftList , CaraftDetail
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     
 ]
 
